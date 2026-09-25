c9596f9c9dfc64c571f6ee0282b24c7aadc970a6
abikananda <abikananda@users.noreply.github.com>
2026-09-26 03:33:43 +0530

    Fix missing JSON exception import

--- src/main/java/com/example/workflow/work/WorkService.java
+++ src/main/java/com/example/workflow/work/WorkService.java
@@ -2,6 +2,7 @@ package com.example.workflow.work;
 
 import com.example.workflow.db.*;
 import com.example.workflow.model.Contracts.*;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
